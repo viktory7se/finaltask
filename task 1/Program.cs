82114c20a7af6855d65973b18df09c24ff6bf2b7
Victoria Sergeeva <viktime.006@gmail.com>
2024-03-16 20:52:37 +0300

    Task1 Complete

--- task 1/Program.cs
+++ task 1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void WriteBetween(int m, int n) 
+{
+    if(m > n) return;
+    Console.WriteLine(m);
+    WriteBetween(++m, n);
+}
+
+int n,m;
+Console.WriteLine("Введите M: ");
+int.TryParse(Console.ReadLine(),out m);
+Console.WriteLine("Введите N: ");
+int.TryParse(Console.ReadLine(), out n);
+
+WriteBetween(m,n);
