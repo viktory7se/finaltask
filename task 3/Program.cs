d7600369f9b0d8ff502fb05dd5d0008baa5e5179
Victoria Sergeeva <viktime.006@gmail.com>
2024-03-16 21:41:06 +0300

    Task 3 Complete

--- task 3/Program.cs
+++ task 3/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Random random = new Random();
+int RandomInt() {
+    return random.Next(0, 1000);
+}
+
+void FillArray(int[] array, int index) {
+    if(index >= array.Length) return;
+    array[index] = RandomInt();
+    FillArray(array, ++index);
+
+}
+
+void PrintIntReverse(int[] array, int index) {
+    if(index < 0) return;
+    Console.WriteLine(array[index]);
+    PrintIntReverse(array, --index);
+}
+int size = 0;
+int[] arr;
+Console.WriteLine("Введите размер массива: ");
+size = Convert.ToInt32(Console.ReadLine());
+
+arr = new int[size];
+FillArray(arr, 0);
+Console.WriteLine("Сгенерированный массив: ");
+Console.WriteLine(string.Join(", ", arr));
+Console.WriteLine("Перевернутый массив: ");
+PrintIntReverse(arr, size - 1);
+
+
+
